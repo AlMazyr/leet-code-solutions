ecd2524bbd245b296d858601dda0221f39e7e200
Alexander Mazyrin <alexandermazyrin@Alexanders-MacBook-Pro.local>
2022-05-26 12:55:14 +0300

    [*] Improve liked list creation

--- python/middle-of-linked-list.py
+++ python/middle-of-linked-list.py
@@ -25,7 +25,7 @@ The number of nodes in the list is in the range [1, 100].
 1 <= Node.val <= 100
 '''
 
-from typing import Optional
+from typing import Optional, List
 
 # Definition for singly-linked list.
 class ListNode:
@@ -36,25 +36,30 @@ class ListNode:
     def __repr__(self) -> str:
         return str(self.val)
 
-class ListHead:
-    def __init__(self, node: ListNode=None):
-        self.node = node
+class LinkedList:
+    def __init__(self, list: List[int]):
+        tail = ListNode()
+        self.head = tail
+        for x in list:
+            tail.next = ListNode(x, None)
+            tail = tail.next
+        self.head = self.head.next
     
-    def __repr__(self):
+    @staticmethod
+    def printList(head: ListNode):
         res = []
-        node = self.node
+        node = head
         while (node != None):
             res += [str(node.val)]
             node = node.next
         res += ['Null']
         return '->'.join(res)
 
-lst = [ListNode(x) for x in [1,2,3,4,5]]
-lst[0].next = lst[1]
-lst[1].next = lst[2]
-lst[2].next = lst[3]
-lst[3].next = lst[4]
-hd = lst[0]
+    def __repr__(self):
+        return LinkedList.printList(self.head)
+
+list = LinkedList([1])
+print(f'input:{list}')
 
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
@@ -69,6 +74,5 @@ class Solution:
         return mid
 
 s = Solution()
-print(f'input:{hd}')
-res = ListHead(s.middleNode(lst[0]))
-print(f'res:{res}')
+res = s.middleNode(list.head)
+print(f'res:{LinkedList.printList(res)}')
